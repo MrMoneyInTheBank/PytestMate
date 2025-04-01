61da3cc5389e021f1853a7ca3136c6228f17fdd6
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-04-01 23:08:55 +0530

    <feat> scaffolded cli commands

--- app/ptm.py
+++ app/ptm.py
@@ -6,3 +6,33 @@ def ptm() -> None:
     pass
 
 
+@click.command()
+def init() -> None:
+    pass
+
+
+@click.command()
+def update() -> None:
+    pass
+
+
+@click.command()
+def test() -> None:
+    pass
+
+
+@click.command()
+def report() -> None:
+    pass
+
+
+@click.command()
+def generate() -> None:
+    pass
+
+
+ptm.add_command(init)
+ptm.add_command(update)
+ptm.add_command(test)
+ptm.add_command(report)
+ptm.add_command(generate)
