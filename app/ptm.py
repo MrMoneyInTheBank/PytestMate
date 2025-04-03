d60137ffba898980418ac2f05a32b7d3fefd5213
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-04-03 19:08:14 +0530

    <test> testing pre-commit hooks by making random change in .py file

--- app/ptm.py
+++ app/ptm.py
@@ -10,6 +10,20 @@ def ptm() -> None:
 
 @click.command()
 def init() -> None:
+    """
+    Verify that the command is run within a Python project.
+
+    This command checks whether the current working directory is a valid Python project.
+    If the verification succeeds, it prints a confirmation message. Otherwise, it prompts
+    the user to run the command inside a Python project.
+
+    Future Enhancements:
+    - Scaffold a test directory structure.
+    - Generate test file placeholders based on existing source code.
+
+    Returns:
+        None
+    """
     current_dir = os.getcwd()
     is_python_project = in_python_project(current_dir)
 
